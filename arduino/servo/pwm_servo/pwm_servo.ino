e80f7d9939eab519b534c808eee74a8f9a3bb1a3
Zuodian Hu <zhu48@wisc.edu>
2017-03-28 09:11:30 -0500

    UDP loopback update to use Heartbeat instead of handshake

---
+++ arduino/servo/pwm_servo/pwm_servo.ino
@@ -0,0 +1,12 @@
+#define PWM_PIN 11
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PWM_PIN, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    analogWrite(PWM_PIN, Serial.read());
+  }
+}
